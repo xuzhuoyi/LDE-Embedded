bfe7658afa0369a450946b31277c197f56a71b60
xuzhuoyi <xzy476386434@vip.qq.com>
2015-04-11 23:45:33 +0800

    Redesign Ldecenter Ui

--- src/ldecenter/form.cpp
+++ src/ldecenter/form.cpp
@@ -6,6 +6,15 @@ Form::Form(QWidget *parent) :
     ui(new Ui::Form)
 {
     ui->setupUi(this);
+    ui->label->setAttribute(Qt::WA_TranslucentBackground, true);
+    ui->label_2->setAttribute(Qt::WA_TranslucentBackground, true);
+    ui->label_3->setAttribute(Qt::WA_TranslucentBackground, true);
+    ui->label_4->setAttribute(Qt::WA_TranslucentBackground, true);
+    ui->label_5->setAttribute(Qt::WA_TranslucentBackground, true);
+    ui->label_6->setAttribute(Qt::WA_TranslucentBackground, true);
+    ui->label_7->setAttribute(Qt::WA_TranslucentBackground, true);
+    ui->label_8->setAttribute(Qt::WA_TranslucentBackground, true);
+    ui->label_9->setAttribute(Qt::WA_TranslucentBackground, true);
 }
 
 Form::~Form()
