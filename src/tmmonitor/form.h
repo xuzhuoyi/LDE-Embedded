f7705d6e0496aec37ba30af3660303b71396db5d
xuzhuoyi <xzy476386434@vip.qq.com>
2015-04-24 23:02:49 +0800

    Remove Old Files

--- src/tmmonitor/form.h
+++ src/tmmonitor/form.h
@@ -15,6 +15,9 @@ public:
     explicit Form(QWidget *parent = 0);
     ~Form();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::Form *ui;
 };
--- src/tmmonitor/main.cpp
+++ src/tmmonitor/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     Form form;
-    form.show();
+    form.showFullScreen();
 
     return app.exec();
 }
